c4d5d7df122b6e955c9a3471e5878cedf842a7f6
tomtom12065 <100985000+tomtom12065@users.noreply.github.com>
2023-07-12 18:32:41 +0000

    saving without css done

--- src/app.js
+++ src/app.js
@@ -8,4 +8,29 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let suite = ["spade", "heart", "club", "diamond"];
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13"
+  ];
+
+  let generator = () => {
+    return suite[Math.floor(Math.random() * suite.length)];
+  };
+  let benerator = () => {
+    return number[Math.floor(Math.random() * number.length)];
+  };
+  document.getElementsByClassName("suite").innerHTML = generator();
+  document.getElementById("middle").innerHTML = benerator();
 };
