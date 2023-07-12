bc738398a71de4018fff1d601f2842518701829b
tomtom12065 <100985000+tomtom12065@users.noreply.github.com>
2023-07-12 23:43:49 +0000

    done... maybe

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-
-  let suite = ["spade", "heart", "club", "diamond"];
+  let colorMapping = {
+    "&#9824;": "black",
+    "&#9827;": "black",
+    "&#9830;": "red",
+    "&#9829;": "red"
+  };
+  let suite = ["&#9824;", "&#9827;", "&#9830;", "&#9829;"];
   let number = [
     "A",
     "2",
@@ -31,9 +29,16 @@ window.onload = function() {
   let benerator = () => {
     return number[Math.floor(Math.random() * number.length)];
   };
-  let suites = document.getElementsByClassName("suite");
-  for (let i = 0; i < suites.length; i++) {
-    suites[i].innerHTML = generator();
-  }
+
+  let topElement = document.getElementById("top");
+  let bottomElement = document.getElementById("bottom");
+
+  let randomSuite = generator();
+  let randomColor = colorMapping[randomSuite];
+  topElement.innerHTML = randomSuite;
+  topElement.style.color = randomColor;
+  bottomElement.innerHTML = randomSuite;
+  bottomElement.style.color = randomColor;
+
   document.getElementById("middle").innerHTML = benerator();
 };
